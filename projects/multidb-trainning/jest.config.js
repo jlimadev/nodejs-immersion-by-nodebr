04339533f037429afc6c9714d1e5727d8eacbd52
Jonathan Lima <jlima.dev@gmail.com>
2021-02-09 09:53:43 -0300

    feat(multi-db): new implementation with jest and eslint

---
+++ projects/multidb-trainning/jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  modulePathIgnorePatterns: ['<rootDir>/coverage/'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage/',
+  ],
+  testMatch: ['<rootDir>/src/**/*.spec.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage/',
+  ],
+
+  watchPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage/',
+  ],
+
+  timers: 'fake',
+  clearMocks: true,
+  resetMocks: false,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js', '!**/jest.config.js', '!src/**/debug.js'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
