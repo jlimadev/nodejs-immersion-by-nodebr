e262d5b634a6a0dec30f13ad6443f6ea7349222f
Jonathan Lima <jlima.dev@gmail.com>
2021-02-11 09:13:57 -0300

    feat(multi-db): config files

--- projects/multidb-trainning/jest.config.js
+++ projects/multidb-trainning/jest.config.js
@@ -1,25 +1,20 @@
 module.exports = {
   modulePathIgnorePatterns: ['<rootDir>/coverage/'],
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/coverage/',
-  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/coverage/'],
   testMatch: ['<rootDir>/src/**/*.spec.js'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/coverage/',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/coverage/'],
 
-  watchPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/coverage/',
-  ],
+  watchPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/coverage/'],
 
   timers: 'fake',
   clearMocks: true,
   resetMocks: false,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js', '!**/jest.config.js', '!src/**/debug.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!**/jest.config.js',
+    '!src/**/debug*.js',
+  ],
   coverageDirectory: './coverage',
   coverageReporters: ['lcov', 'text'],
   coverageThreshold: {
