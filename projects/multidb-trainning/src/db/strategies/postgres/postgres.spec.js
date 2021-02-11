b6a09e04fe9a92fdef005f6d5ccf55f910fad350
Jonathan Lima <jlima.dev@gmail.com>
2021-02-11 16:16:16 -0300

    feat(multi-db): test to postgres constructor

--- projects/multidb-trainning/src/db/strategies/postgres/postgres.spec.js
+++ projects/multidb-trainning/src/db/strategies/postgres/postgres.spec.js
@@ -1,5 +1,51 @@
-describe('Postgres Strategy', () => {
-  it('Should test', () => {
-    expect(true).toBe(true);
+const Postgres = require('./postgres');
+
+// Factory SUT = System Under Test
+const makeSut = () => {
+  const Sut = Postgres;
+  const connection = Sut.connect();
+  const schema = 'heroes';
+
+  return { Sut, connection, schema };
+};
+
+describe('Constructor', () => {
+  it('Should throw a new error if connection is not informed', async () => {
+    // Arrange
+    const { Sut, schema } = makeSut();
+
+    // Act
+    const act = () => {
+      new Sut('', schema);
+    };
+
+    // Assert
+    expect(act).toThrow('You must inject the dependecies');
+  });
+
+  it('Should throw a new error if the schema is not informed', async () => {
+    // Arrange
+    const { Sut, connection } = makeSut();
+
+    // Act
+    const act = () => {
+      new Sut(connection, '');
+    };
+
+    // Assert
+    expect(act).toThrow('You must inject the dependecies');
+  });
+
+  it('Should return an object that contains the connection infos', async () => {
+    // Arrange
+    const { Sut, connection, schema } = makeSut();
+
+    // Act
+    const postgres = new Sut(connection, schema);
+    const keys = Object.keys(postgres);
+
+    // Assert
+    expect(postgres).toBeInstanceOf(Object);
+    expect(keys).toEqual(['_connection', '_schema']);
   });
 });
