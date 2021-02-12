441f5f5e62bfe1d42a7ca6e07d5c33cf09d9b75f
Jonathan Lima <jlima.dev@gmail.com>
2021-02-12 18:56:06 -0300

    feat(multi-db): debug postgres

--- projects/multidb-trainning/src/db/strategies/postgres/debugPostgres.js
+++ projects/multidb-trainning/src/db/strategies/postgres/debugPostgres.js
@@ -3,7 +3,7 @@ const HeroesSchema = require('./schemas/heroesSchema');
 const ContextStrategy = require('../context/ContextStrategy');
 
 const call = async () => {
-  const sampleObject = { name: 'Ruru', power: 'Muito Gordin' };
+  const sampleObject = { name: 'Ruru', power: 'Gordin' };
 
   const connection = Postgres.connect();
 
@@ -11,18 +11,21 @@ const call = async () => {
 
   const context = new ContextStrategy(new Postgres(connection, model));
 
-  /* const createdObject = await context.create(sampleObject);
-  
+  // const createdObject = await context.create(sampleObject);
+
+  /* 
   const updatedObject = await context.update(
     '19cb7c30-da60-45e4-b6ea-0a1f889da84c',
     sampleObject,
-  );
-  
+  ); 
+  */
+
+  /* 
   const deletedObject = await context.delete(
     '19cb7c30-da60-45e4-b6ea-0a1f889da84c',
-  ); */
-
-  const returnValues = await context.read();
+  );
+ */
+  const returnValues = await context.read({ name: 'Bomuto' });
 
   console.log(returnValues);
   Postgres.disconnect(connection);
