d08ed6f2b2fdfdb238222f04924b6f37e01105bd
Jonathan Lima <jlima.dev@gmail.com>
2021-02-14 10:50:30 -0300

    feat(multi-db): add tests to delete and exports method

--- projects/multidb-trainning/src/db/strategies/postgres/postgres.spec.js
+++ projects/multidb-trainning/src/db/strategies/postgres/postgres.spec.js
@@ -5,6 +5,7 @@ const makeSut = () => {
   const Sut = Postgres;
   const connection = Sut.connect();
   const schema = 'heroes';
+  const mockUUID = '19cb7c30-da60-45e4-b6ea-0a1f889da84c';
   const mockInput = { name: 'any name', power: 'any power' };
   const mockUpdate = { name: 'any other name', power: 'any other power' };
   const mockedReturnValue = { id: 1, ...mockInput };
@@ -14,12 +15,30 @@ const makeSut = () => {
     Sut,
     connection,
     schema,
+    mockUUID,
     mockInput,
     mockUpdate,
     mockedReturnValue,
     errorMessage,
   };
 };
+
+describe('Postgres exports', () => {
+  it('Should be instance of object', () => {
+    const { Sut, connection, schema } = makeSut();
+    const postgres = new Sut(connection, schema);
+
+    expect(postgres).toBeInstanceOf(Object);
+    expect(postgres.create).toBeInstanceOf(Function);
+    expect(postgres.read).toBeInstanceOf(Function);
+    expect(postgres.update).toBeInstanceOf(Function);
+    expect(postgres.delete).toBeInstanceOf(Function);
+    expect(postgres.isConnected).toBeInstanceOf(Function);
+    expect(Sut.connect).toBeInstanceOf(Function);
+    expect(Sut.disconnect).toBeInstanceOf(Function);
+    expect(Sut.defineModel).toBeInstanceOf(Function);
+  });
+});
 
 describe('Postgres Constructor', () => {
   it('Should throw a new error if connection is not informed', async () => {
@@ -212,6 +231,69 @@ describe('Postgres Methods', () => {
       const result = await postgres.update('nonExistingId', mockUpdate);
 
       expect(result).toStrictEqual(expectedResponse);
+    });
+  });
+
+  describe('delete', () => {
+    it('Should return an error if delete fails or rejects', async () => {
+      const { Sut, connection, schema, mockUUID, errorMessage } = makeSut();
+      const postgres = new Sut(connection, schema);
+
+      postgres.delete = jest.fn(() => Promise.reject(new Error(errorMessage)));
+
+      const act = async () => {
+        await postgres.delete(mockUUID);
+      };
+
+      await expect(act).rejects.toThrow(errorMessage);
+    });
+
+    it('Should return an error if is not an UUID', async () => {
+      const { Sut, connection, schema, errorMessage } = makeSut();
+      const postgres = new Sut(connection, schema);
+
+      postgres.delete = jest.fn(() => Promise.reject(new Error(errorMessage)));
+
+      const act = async () => {
+        await postgres.delete('InvalidUUID');
+      };
+
+      await expect(act).rejects.toThrow(errorMessage);
+    });
+
+    it('Should return an error if id is missing', async () => {
+      const { Sut, connection, schema, errorMessage } = makeSut();
+      const postgres = new Sut(connection, schema);
+
+      postgres.delete = jest.fn(() => Promise.reject(new Error(errorMessage)));
+
+      const act = async () => {
+        await postgres.delete(undefined);
+      };
+
+      await expect(act).rejects.toThrow(errorMessage);
+    });
+
+    it('Should return 1 if deleted successfuly', async () => {
+      const { Sut, connection, schema, mockUUID } = makeSut();
+      const postgres = new Sut(connection, schema);
+
+      postgres.delete = jest.fn().mockReturnValue(1);
+
+      const result = await postgres.delete(mockUUID);
+
+      expect(result).toStrictEqual(1);
+    });
+
+    it('Should return 0 if nothing is deleted', async () => {
+      const { Sut, connection, schema, mockUUID } = makeSut();
+      const postgres = new Sut(connection, schema);
+
+      postgres.delete = jest.fn().mockReturnValue(0);
+
+      const result = await postgres.delete(mockUUID);
+
+      expect(result).toStrictEqual(0);
     });
   });
 });
