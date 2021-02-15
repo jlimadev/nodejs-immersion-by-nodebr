9d4174f92fc2998b29d2f145a32aa9e97fbda1c0
Jonathan Lima <jlima.dev@gmail.com>
2021-02-15 15:43:43 -0300

    feat(mongo-db): MongoDB index - wip

---
+++ projects/multidb-trainning/src/db/strategies/mongodb/mongodb.js
@@ -0,0 +1,46 @@
+const ICrud = require('../interfaces/ICrud');
+const Mongoose = require('mongoose');
+
+class MongoDB extends ICrud {
+  constructor(connection, schema) {
+    super();
+
+    if (!connection || !schema)
+      throw new Error('You must inject the dependecies');
+
+    this._connection = connection;
+    this._schema = schema;
+  }
+
+  // async create(){};
+  // async read(){};
+  // async update(){};
+  // async delete(){};
+  async isConnected() {}
+
+  static async connect() {
+    const uri = 'mongodb://jlimadev:secretpass@localhost:27017/heroes';
+
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    Mongoose.connect(uri, options);
+
+    const connection = Mongoose.connection;
+
+    connection.once('open', () => {
+      console.log('Database is running');
+      return true;
+    });
+  }
+
+  static async disconnect() {
+    Mongoose.connection.close();
+    Mongoose.disconnect();
+    console.log('Diconnected from database');
+  }
+}
+
+module.exports = MongoDB;
