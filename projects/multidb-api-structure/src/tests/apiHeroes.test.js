995f0736073bfd9e935d0deeb5cf345b15492e52
Jonathan Lima <jlima.dev@gmail.com>
2021-02-24 09:26:38 -0300

    feat(happijs): wip list tests

--- projects/multidb-api-structure/src/tests/apiHeroes.test.js
+++ projects/multidb-api-structure/src/tests/apiHeroes.test.js
@@ -3,68 +3,70 @@ const api = require('../api');
 
 let app = {};
 
-describe.only('Test to api hereoes', () => {
+describe('Test to api hereoes', () => {
   before(async () => {
     app = await api;
   });
 
-  it('Should list the heroes on /heroes', async () => {
-    const result = await app.inject({
-      method: 'GET',
-      url: '/heroes',
+  describe('list', () => {
+    it('Should list the heroes on /heroes', async () => {
+      const result = await app.inject({
+        method: 'GET',
+        url: '/heroes',
+      });
+
+      const { statusCode, payload } = result;
+
+      const data = JSON.parse(payload);
+
+      assert.strictEqual(statusCode, 200);
+      assert.ok(Array.isArray(data));
     });
 
-    const { statusCode, payload } = result;
+    it('Should return <= 10 items from database', async () => {
+      const LIMIT = 10;
+      const SKIP = 0;
+      const result = await app.inject({
+        method: 'GET',
+        url: `/heroes?skip=${SKIP}&limit=${LIMIT}`,
+      });
 
-    const data = JSON.parse(payload);
+      const { statusCode, payload } = result;
+      const data = JSON.parse(payload);
 
-    assert.strictEqual(statusCode, 200);
-    assert.ok(Array.isArray(data));
-  });
-
-  it('Should return <= 10 items from database', async () => {
-    const LIMIT = 10;
-    const SKIP = 0;
-    const result = await app.inject({
-      method: 'GET',
-      url: `/heroes?skip=${SKIP}&limit=${LIMIT}`,
+      assert.strictEqual(statusCode, 200);
+      assert.ok(Array.isArray(data));
+      assert.ok(data.length <= LIMIT);
     });
 
-    const { statusCode, payload } = result;
-    const data = JSON.parse(payload);
+    it('Should fail if limit or skip are string', async () => {
+      const LIMIT = 'ANYWRONG';
+      const SKIP = 'ANYWRONG';
+      const result = await app.inject({
+        method: 'GET',
+        url: `/heroes?skip=${SKIP}&limit=${LIMIT}`,
+      });
 
-    assert.strictEqual(statusCode, 200);
-    assert.ok(Array.isArray(data));
-    assert.ok(data.length <= LIMIT);
-  });
-
-  it('Should fail if limit or skip are string', async () => {
-    const LIMIT = 'ANYWRONG';
-    const SKIP = 'ANYWRONG';
-    const result = await app.inject({
-      method: 'GET',
-      url: `/heroes?skip=${SKIP}&limit=${LIMIT}`,
+      const { statusCode, statusMessage } = result;
+      assert.strictEqual(statusCode, 500);
+      assert.strictEqual(statusMessage, 'Internal Server Error');
     });
 
-    const { statusCode, statusMessage } = result;
-    assert.strictEqual(statusCode, 500);
-    assert.strictEqual(statusMessage, 'Internal Server Error');
-  });
+    it('Should filter by name', async () => {
+      const LIMIT = 10;
+      const SKIP = 0;
+      const NAME = 'Ruru';
+      const result = await app.inject({
+        method: 'GET',
+        url: `/heroes?skip=${SKIP}&limit=${LIMIT}&name=${NAME}`,
+      });
 
-  it('Should filter by name', async () => {
-    const LIMIT = 10;
-    const SKIP = 0;
-    const NAME = 'Ruru';
-    const result = await app.inject({
-      method: 'GET',
-      url: `/heroes?skip=${SKIP}&limit=${LIMIT}&name=${NAME}`,
+      const { statusCode, payload } = result;
+      const data = JSON.parse(payload);
+
+      assert.strictEqual(data[0].name, NAME);
+      assert.strictEqual(statusCode, 200);
+      assert.ok(Array.isArray(data));
     });
-
-    const { statusCode, payload } = result;
-    const data = JSON.parse(payload);
-
-    assert.strictEqual(data[0].name, NAME);
-    assert.strictEqual(statusCode, 200);
-    assert.ok(Array.isArray(data));
   });
 });
